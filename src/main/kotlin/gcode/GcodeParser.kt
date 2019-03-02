839d80a329a9ef42c3e4635db137d4b23a4b44e8
standa <ondra.stanicek@gmail.com>
2019-03-02 23:37:14 +0100

    first implementation of GCode stringifying, will probably change, but streaming to plotter working

--- src/main/kotlin/gcode/GcodeParser.kt
+++ src/main/kotlin/gcode/GcodeParser.kt
@@ -4,12 +4,46 @@ import java.io.Closeable
 import java.io.File
 import java.lang.Exception
 import java.lang.NumberFormatException
+import java.lang.StringBuilder
 
 typealias GCode = Map<Char, Float>
 
+const val decimalPlaces: Int = 3
+
 fun GCode.stringify(): String {
-    return this.toString() + "\n"
+    val builder = StringBuilder(this.size * 6) // one letter char and max 5 chars for number
+
+    val skipKeys = listOf('G', 'X', 'Y', 'Z', 'F')
+    if (this.contains('G')) {
+        builder.appendField(this, 'G')
+        builder.appendField(this, 'X')
+        builder.appendField(this, 'Y')
+        builder.appendField(this, 'Z')
+        builder.appendField(this, 'F')
+    }
+
+    for (key in this.keys) {
+        if (key in skipKeys) {
+            continue
+        }
+        builder.appendField(this, key)
+    }
+
+    builder.append("\n")
+    return builder.toString()
 }
+
+fun StringBuilder.appendField(gcode: GCode, key: Char) {
+    if(gcode.containsKey(key)) {
+        append(key)
+        append(gcode.getValue(key).roundTo(decimalPlaces))
+    }
+}
+
+fun Float.roundTo(decimalPlaces: Int): String {
+    return "%.${decimalPlaces}f".format(this)
+}
+
 
 class GcodeParser(private val gcodeFile: File) : Iterable<GCode?>, Iterator<GCode?>, Closeable {
 
